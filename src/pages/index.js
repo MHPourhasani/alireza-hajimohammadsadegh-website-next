51d9fc57bedb04b50952987502ac347a2a96ba50
Mohammad Hasan Pouhasani <mhpourhasani.dev@gmail.com>
2023-04-06 07:46:00 +0430

    create new next app

---
+++ src/pages/index.js
@@ -0,0 +1,25 @@
+import Head from 'next/head';
+
+// components
+import Layout from '@/components/Layout/Layout';
+import Skills from '@/components/Skills/Skills';
+import Projects from '@/components/Projects/Projects';
+
+export default function Home() {
+	return (
+		<>
+				<Head>
+					<title>علیرضا حاجی محمد صادق</title>
+					<meta name='description' content='علیرضا مرجانی هستم. گرافیست و ادیتور' />
+					<meta name='viewport' content='width=device-width, initial-scale=1' />
+					<meta name='theme-color' content='#21252A' />
+					<link rel='icon' href='/favicon.ico' />
+				</Head>
+
+			<Layout>
+				<Skills />
+				<Projects />
+			</Layout>
+		</>
+	);
+}
