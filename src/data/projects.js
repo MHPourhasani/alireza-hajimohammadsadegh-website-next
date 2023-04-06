7f47ffa2c5467e092195c868926d357f3538d958
Mohammad Hasan Pouhasani <mhpourhasani.dev@gmail.com>
2023-04-06 08:08:36 +0430

    created projects data

---
+++ src/data/projects.js
@@ -0,0 +1,85 @@
+import parchame_entegham from '../images/projects/soroud cover/Parchame Entegham-cover.jpg';
+import shokouhe_heydar from '../images/projects/soroud cover/Shokouhe Heydar-cover.jpg';
+import najaf_ta_karbala from '../images/projects/soroud cover/Najaf Ta Karbala-cover.jpg';
+import eydaneh from '../images/projects/soroud cover/Eydaneh-cover.jpg';
+import sobhe_jamaat from '../images/projects/soroud cover/Sobhe Jamaat-cover.jpg';
+
+export const projects = {
+	سرود: [
+		{
+			id: 1,
+			name: 'پرچم انتقام',
+			posterSrc: parchame_entegham,
+			href: 'https://1drv.ms/u/s!ArNeogtEVeNZiyAc34exXf6RtLUu?e=Xxh3Hn',
+		},
+		{
+			id: 2,
+			name: 'شکوه حیدر',
+			posterSrc: shokouhe_heydar,
+			href: 'https://1drv.ms/u/s!ArNeogtEVeNZix09yhp6NclZz_y_?e=AkK9yi',
+		},
+		{
+			id: 3,
+			name: 'نجف تا کربلا',
+			posterSrc: najaf_ta_karbala,
+			href: 'https://1drv.ms/u/s!ArNeogtEVeNZiyG_66q4VC1qVOJn?e=cBtIup',
+		},
+		{
+			id: 4,
+			name: 'صدای قدر',
+			posterSrc: '',
+			href: 'https://1drv.ms/u/s!ArNeogtEVeNZix8T3US87QCgfP7F?e=ETmOzD',
+		},
+		{
+			id: 5,
+			name: 'قرن جدید',
+			posterSrc: '',
+			href: 'https://1drv.ms/u/s!ArNeogtEVeNZix497xXXbVN85CyL?e=PHjArW',
+		},
+		{
+			id: 6,
+			name: 'صبح جماعت',
+			posterSrc: sobhe_jamaat,
+			href: 'https://1drv.ms/u/s!ArNeogtEVeNZiyQCyLbZvYqVL0Vp?e=8Bs78K',
+		},
+		{
+			id: 7,
+			name: 'عیدانه',
+			posterSrc: eydaneh,
+			href: 'https://1drv.ms/u/s!ArNeogtEVeNZiyIjDVkvJTff_yrs?e=oejN6x',
+		},
+		{
+			id: 8,
+			name: 'دست کریم',
+			posterSrc: '',
+			href: 'https://1drv.ms/u/s!ArNeogtEVeNZiyUGvYt_pHxbz4ww?e=bD9sRd',
+		},
+	],
+
+	'اجرا ها': [
+		{
+			id: 1,
+			name: 'انتخابات 1400',
+			posterSrc: '',
+			href: 'https://1drv.ms/v/s!ArNeogtEVeNZiynoZiMQtLg49dNm?e=8Gqtni',
+		},
+		{
+			id: 2,
+			name: 'عید غدیر',
+			posterSrc: '',
+			href: '',
+		},
+		{
+			id: 3,
+			name: 'پرچم انتقام (پخش شده در شبکه 3)',
+			posterSrc: '',
+			href: 'https://1drv.ms/v/s!ArNeogtEVeNZiy35WVHXfv8vcL7H?e=tncZid',
+		},
+		{
+			id: 4,
+			name: 'صدای قدر (پخش شده در شبکه 5)',
+			posterSrc: '',
+			href: '',
+		},
+	],
+};
