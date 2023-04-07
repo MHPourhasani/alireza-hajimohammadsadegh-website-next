336822b4467f49a0bb70a9de80fa9362e5ccb44e
Mohammad Hasan Pouhasani <mhpourhasani.dev@gmail.com>
2023-04-07 18:16:50 +0430

    changed description meta tag content

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,13 +8,13 @@ import Projects from '@/components/Projects/Projects';
 export default function Home() {
 	return (
 		<>
-				<Head>
-					<title>علیرضا حاجی محمد صادق</title>
-					<meta name='description' content='علیرضا مرجانی هستم. گرافیست و ادیتور' />
-					<meta name='viewport' content='width=device-width, initial-scale=1' />
-					<meta name='theme-color' content='#21252A' />
-					<link rel='icon' href='/favicon.ico' />
-				</Head>
+			<Head>
+				<title>علیرضا حاجی محمد صادق</title>
+				<meta name='description' content='' />
+				<meta name='viewport' content='width=device-width, initial-scale=1' />
+				<meta name='theme-color' content='#21252A' />
+				<link rel='icon' href='/favicon.ico' />
+			</Head>
 
 			<Layout>
 				<Skills />
