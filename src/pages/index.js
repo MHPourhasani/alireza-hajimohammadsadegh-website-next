16883f9a387aed53bc763f65c4610dcb9bfb52ae
Mohammad Hasan Pouhasani <mhpourhasani.dev@gmail.com>
2023-04-08 02:36:58 +0430

    changed description meta tag content

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
 		<>
 			<Head>
 				<title>علیرضا حاجی محمد صادق</title>
-				<meta name='description' content='' />
+				<meta name='description' content='علیرضا حاجی محمدصادق هستم.' />
 				<meta name='viewport' content='width=device-width, initial-scale=1' />
 				<meta name='theme-color' content='#21252A' />
 				<link rel='icon' href='/favicon.ico' />
