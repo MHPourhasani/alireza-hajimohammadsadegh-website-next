0154a41c83dc6f9429dabcca498b830cf7d0eca0
Mohammad Hasan Pouhasani <mhpourhasani.dev@gmail.com>
2023-04-09 00:21:28 +0430

    updated projects data

--- src/data/projects.js
+++ src/data/projects.js
@@ -6,55 +6,67 @@ import sobhe_jamaat from '../images/projects/soroud cover/Sobheh Jamaat-cover.jp
 import eydaneh from '../images/projects/soroud cover/Eydaneh-cover.jpg';
 import daste_karim from '../images/projects/soroud cover/Daste Karim-cover.jpg';
 
+import entekhabat_1400_poster from '../images/projects/soroud ejra/Entekhabat 1400-poster.jpg';
+import parchame_entegham_poster from '../images/projects/soroud ejra/Parchame Entegham-poster.jpg';
+import sedaye_ghadr_poster from '../images/projects/soroud ejra/Sedaye Ghadr-poster.jpg';
+
 export const projects = {
 	سرود: [
 		{
 			id: 1,
 			name: 'پرچم انتقام',
 			posterSrc: parchame_entegham,
-			href: 'https://1drv.ms/u/s!ArNeogtEVeNZiyAc34exXf6RtLUu?e=Xxh3Hn',
+			// href: 'https://1drv.ms/u/s!ArNeogtEVeNZiyAc34exXf6RtLUu?e=Xxh3Hn',
+			href: 'https://drive.google.com/file/d/1sNuhCumkpVjDaVLedGLgtxbRtZnKXHd5/view?usp=share_link',
 		},
 		{
 			id: 2,
 			name: 'شکوه حیدر',
 			posterSrc: shokouhe_heydar,
-			href: 'https://1drv.ms/u/s!ArNeogtEVeNZix09yhp6NclZz_y_?e=AkK9yi',
+			// href: 'https://1drv.ms/u/s!ArNeogtEVeNZix09yhp6NclZz_y_?e=AkK9yi',
+			href: 'https://drive.google.com/file/d/16h6pUpRStC3oUfsr6u5BUOiS3oOPymRM/view?usp=share_link',
 		},
 		{
 			id: 3,
 			name: 'نجف تا کربلا',
 			posterSrc: najaf_ta_karbala,
-			href: 'https://1drv.ms/u/s!ArNeogtEVeNZiyG_66q4VC1qVOJn?e=cBtIup',
+			// href: 'https://1drv.ms/u/s!ArNeogtEVeNZiyG_66q4VC1qVOJn?e=cBtIup',
+			href: 'https://drive.google.com/file/d/11CmCvcSx9vJsBuG_T_-d3KM4Y5Up-czV/view?usp=share_link',
 		},
 		{
 			id: 4,
 			name: 'صدای قدر',
 			posterSrc: sedaye_ghadr,
-			href: 'https://1drv.ms/u/s!ArNeogtEVeNZix8T3US87QCgfP7F?e=ETmOzD',
+			// href: 'https://1drv.ms/u/s!ArNeogtEVeNZix8T3US87QCgfP7F?e=ETmOzD',
+			href: 'https://drive.google.com/file/d/1GokExcu_iojnD_vfdb2P0fqyY2oS1_zP/view?usp=share_link',
 		},
 		{
 			id: 5,
 			name: 'قرن جدید',
 			posterSrc: '',
-			href: 'https://1drv.ms/u/s!ArNeogtEVeNZix497xXXbVN85CyL?e=PHjArW',
+			// href: 'https://1drv.ms/u/s!ArNeogtEVeNZix497xXXbVN85CyL?e=PHjArW',
+			href: 'https://drive.google.com/file/d/1In9M4VexOcQ11fJrNvjxV9QcRAE9kXhf/view?usp=share_link',
 		},
 		{
 			id: 6,
 			name: 'صبح جماعت',
 			posterSrc: sobhe_jamaat,
-			href: 'https://1drv.ms/u/s!ArNeogtEVeNZiyQCyLbZvYqVL0Vp?e=CBOAk3',
+			// href: 'https://1drv.ms/u/s!ArNeogtEVeNZiyQCyLbZvYqVL0Vp?e=CBOAk3',
+			href: '',
 		},
 		{
 			id: 7,
 			name: 'عیدانه',
 			posterSrc: eydaneh,
-			href: 'https://1drv.ms/u/s!ArNeogtEVeNZiyIjDVkvJTff_yrs?e=M6IE2a',
+			// href: 'https://1drv.ms/u/s!ArNeogtEVeNZiyIjDVkvJTff_yrs?e=M6IE2a',
+			href: 'https://drive.google.com/file/d/1558Dky-cVRJ5ubEimWR57vIwEt-WoH-E/view?usp=share_link',
 		},
 		{
 			id: 8,
 			name: 'دست کریم',
 			posterSrc: daste_karim,
-			href: 'https://1drv.ms/u/s!ArNeogtEVeNZiyUGvYt_pHxbz4ww?e=bD9sRd',
+			// href: 'https://1drv.ms/u/s!ArNeogtEVeNZiyUGvYt_pHxbz4ww?e=bD9sRd',
+			href: 'https://drive.google.com/file/d/1Npl8K9zUJoBj02YC32g58TETSVZC2h9P/view?usp=share_link',
 		},
 	],
 
@@ -62,8 +74,9 @@ export const projects = {
 		{
 			id: 1,
 			name: 'انتخابات 1400',
-			posterSrc: '',
-			href: 'https://1drv.ms/v/s!ArNeogtEVeNZiynoZiMQtLg49dNm?e=8Gqtni',
+			posterSrc: entekhabat_1400_poster,
+			// href: 'https://1drv.ms/v/s!ArNeogtEVeNZiynoZiMQtLg49dNm?e=8Gqtni',
+			href: 'https://drive.google.com/file/d/1JvBl5KqIb9Ayja3jgTJzVs8aCd4kum8g/view?usp=share_link',
 		},
 		{
 			id: 2,
@@ -74,14 +87,15 @@ export const projects = {
 		{
 			id: 3,
 			name: 'پرچم انتقام (پخش شده در شبکه 3)',
-			posterSrc: '',
-			href: 'https://1drv.ms/v/s!ArNeogtEVeNZiy35WVHXfv8vcL7H?e=tncZid',
+			posterSrc: parchame_entegham_poster,
+			// href: 'https://1drv.ms/v/s!ArNeogtEVeNZiy35WVHXfv8vcL7H?e=tncZid',
+			href: 'https://drive.google.com/file/d/1_QnrYCJLbaE483ObdhC1bvy3Wl5AlbWO/view?usp=share_link',
 		},
 		{
 			id: 4,
 			name: 'صدای قدر (پخش شده در شبکه 5)',
-			posterSrc: '',
-			href: '',
+			posterSrc: sedaye_ghadr_poster,
+			href: 'https://drive.google.com/file/d/1eB15NZ5U0sGK84_15EpIOC5BPCT7edRS/view?usp=share_link',
 		},
 	],
 };
