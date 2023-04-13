453a2332d72d1c336e9478d8963dd80492723f52
Mohammad Hasan Pouhasani <mhpourhasani.dev@gmail.com>
2023-04-13 18:58:31 +0430

    updated projects data

--- src/data/projects.js
+++ src/data/projects.js
@@ -15,6 +15,22 @@ import haram_abd_ol_azim from '../images/projects/soroud ejra/haram abd-ol-azim.
 
 import rezvan_1 from '../images/projects/rezvan/rezvan-1.jpg';
 import rezvan_2 from '../images/projects/rezvan/rezvan-2.jpg';
+import rezvan_4 from '../images/projects/rezvan/rezvan-4.jpg';
+import rezvan_5 from '../images/projects/rezvan/rezvan-5.jpg';
+import rezvan_6 from '../images/projects/rezvan/rezvan-6.jpg';
+import rezvan_7 from '../images/projects/rezvan/rezvan-7.jpg';
+import rezvan_8 from '../images/projects/rezvan/rezvan-8.jpg';
+import rezvan_9 from '../images/projects/rezvan/rezvan-9.jpg';
+import rezvan_10 from '../images/projects/rezvan/rezvan-10.jpg';
+import rezvan_11 from '../images/projects/rezvan/rezvan-11.jpg';
+import rezvan_12 from '../images/projects/rezvan/rezvan-12.jpg';
+import rezvan_13 from '../images/projects/rezvan/rezvan-13.jpg';
+import rezvan_14 from '../images/projects/rezvan/rezvan-14.jpg';
+import rezvan_15 from '../images/projects/rezvan/rezvan-15.jpg';
+import rezvan_16 from '../images/projects/rezvan/rezvan-16.jpg';
+import rezvan_17 from '../images/projects/rezvan/rezvan-17.jpg';
+import rezvan_18 from '../images/projects/rezvan/rezvan-18.jpg';
+import rezvan_19 from '../images/projects/rezvan/rezvan-19.jpg';
 
 export const projects = {
 	سرود: [
@@ -129,6 +145,108 @@ export const projects = {
 			name: 'اختتامیه سال 1394 رضوان',
 			posterSrc: rezvan_1,
 			href: 'https://drive.google.com/file/d/1u3uAG9416H2b6syxiY265rSON9A7t_z9/view?usp=share_link',
-		}
+		},
+		{
+			id: 2,
+			name: 'حضور گروه رضوان در هیئت',
+			posterSrc: rezvan_2,
+			href: 'https://drive.google.com/file/d/1XeuzxSpY9zBSDjFVcRoOfjguxC5c_fKT/view?usp=share_link',
+		},
+		{
+			id: 3,
+			name: 'برگزاری فوتبال به صورت هفتگی',
+			posterSrc: rezvan_4,
+			href: 'https://drive.google.com/file/d/1ALY5yb85GOYMzyB6Y43RavceVdpvik8n/view?usp=share_link',
+		},
+		{
+			id: 4,
+			name: 'برگزاری کلاس احکام',
+			posterSrc: rezvan_5,
+			href: 'https://drive.google.com/file/d/1BX84uQDL986wNqn6yN7iv2VDyBuEytV8/view?usp=share_link',
+		},
+		{
+			id: 5,
+			name: 'برگزاری استخر',
+			posterSrc: rezvan_6,
+			href: 'https://drive.google.com/file/d/1TuDWOJ3ggf9xcYEUTMbHspi75lvYrukX/view?usp=share_link',
+		},
+		{
+			id: 6,
+			name: 'برگزاری سفره افطاری در ماه رمضان',
+			posterSrc: rezvan_7,
+			href: 'https://drive.google.com/file/d/1PXGAqopqOKGlpZaQ7EjDItdHxFD_Da_K/view?usp=share_link',
+		},
+		{
+			id: 7,
+			name: 'اردوی زیارتی حضرت عبدالعظیم (ع)',
+			posterSrc: rezvan_8,
+			href: 'https://drive.google.com/file/d/16MlciTAaZwc47BiaRabF86ChlTHSImB-/view?usp=share_link',
+		},
+		{
+			id: 8,
+			name: 'برگزاری برنامه تابستانه در پارک آلاله',
+			posterSrc: rezvan_9,
+			href: 'https://drive.google.com/file/d/1P07ui5_ysyHKFbpZc7qV7jWO0lJiV36Y/view?usp=share_link',
+		},
+		{
+			id: 9,
+			name: 'برگزاری برنامه تابستانه در پارک آلاله',
+			posterSrc: rezvan_10,
+			href: 'https://drive.google.com/file/d/1YQ067TKN97hupIDR_LTTImVl5inxpZWS/view?usp=share_link',
+		},
+		{
+			id: 10,
+			name: 'عکس دسته جمعی',
+			posterSrc: rezvan_11,
+			href: 'https://drive.google.com/file/d/1FFoieR4_KAvbcu42ORQpgmCOMTq3ZLB7/view?usp=share_link',
+		},
+		{
+			id: 11,
+			name: 'برگزاری کلاس اخلاق',
+			posterSrc: rezvan_12,
+			href: 'https://drive.google.com/file/d/1ES8Tdd7DQ9vLkaYxaBhaMsgoWs_bJzS6/view?usp=share_link',
+		},
+		{
+			id: 12,
+			name: 'انتشار برنامه تابستانه طرح رضوان در روزنامه قانون',
+			posterSrc: rezvan_13,
+			href: 'https://drive.google.com/file/d/1qHvsSrk2DaxdpDI49woAMwyYupqN58Fp/view?usp=share_link',
+		},
+		{
+			id: 13,
+			name: 'برگزاری برنامه تابستانه در پارک آلاله',
+			posterSrc: rezvan_14,
+			href: 'https://drive.google.com/file/d/1_SkQnEQh0NTx-UCHLibFeieAHoq0cbtl/view?usp=share_link',
+		},
+		{
+			id: 14,
+			name: 'برگزاری اردوی زیارتی مشهد مقدس',
+			posterSrc: rezvan_15,
+			href: 'https://drive.google.com/file/d/1TtY717wJtQYtLOhYv25rRGyqX8P6NiYr/view?usp=share_link',
+		},
+		{
+			id: 15,
+			name: 'عکس دسته جمعی با حاج سعید حدادیان',
+			posterSrc: rezvan_16,
+			href: 'https://drive.google.com/file/d/1aHiBpUViApIQfy3lC2BkhX756OxOTVhb/view?usp=share_link',
+		},
+		{
+			id: 16,
+			name: 'تمرین گروه سرود رضوان (بهمن 96)',
+			posterSrc: rezvan_17,
+			href: 'https://drive.google.com/file/d/19La4XX_iIh9Tz_C_N481XI8FMUjC2ebS/view?usp=share_link',
+		},
+		{
+			id: 17,
+			name: 'برگزاری اردوی زیارتی مشهد مقدس',
+			posterSrc: rezvan_18,
+			href: 'https://drive.google.com/file/d/1-nw5d1pnVCbb-iJb7C5FzSqlV6Og9gSf/view?usp=share_link',
+		},
+		{
+			id: 18,
+			name: 'برگزاری اردوی زیارتی مشهد مقدس',
+			posterSrc: rezvan_19,
+			href: 'https://drive.google.com/file/d/1yvZyhGUfLTekJkGHSP29AXOiyyafVeCf/view?usp=share_link',
+		},
 	],
 };
